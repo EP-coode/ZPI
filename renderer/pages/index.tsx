bd065135cbff7e5208d595bd642091490eb7d091
Ernest Przybył <ernestprzybyl@gmail.com>
2022-10-14 15:12:40 +0200

    add typography plugin, rm unnecesary imports

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -2,11 +2,8 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 
-import styles from "../styles/Home.module.css";
-import exampleImage from "../public/assets/examlpe.jpg";
-
 const Home: NextPage = () => {
-  return <div className={styles.container}>INDEX PAGE</div>;
+  return <div>INDEX PAGE</div>;
 };
 
 export default Home;
