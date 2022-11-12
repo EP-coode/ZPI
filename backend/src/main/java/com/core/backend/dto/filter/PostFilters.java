a30a9ae0bfa5ddec89d677cf2984fb26aa91fe79
Kuba <nadolnyjakub@wp.pl>
2022-11-12 15:43:50 +0100

    Add PostFilters

---
+++ backend/src/main/java/com/core/backend/dto/filter/PostFilters.java
@@ -0,0 +1,13 @@
+package com.core.backend.dto.filter;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PostFilters {
+    private String[] tagNames;
+    private String categoryGroup;
+    private String category;
+    private long creatorId;
+}
