d8a430ddb298cc2516cec280c81fe24b4d0aa2e3
Ernest Przybył <ernestprzybyl@gmail.com>
2022-11-16 13:33:13 +0100

    add bread crumbs

--- renderer/pages/posts/category/[category_group]/[category]/index.tsx
+++ renderer/pages/posts/category/[category_group]/[category]/index.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from "next";
 import { useRouter } from "next/router";
 import React from "react";
+import { BreadCrumbs } from "../../../../../src/components/BreadCrumbs";
 import CategoryTree from "../../../../../src/components/CategoryTree";
 import PostFilterBox from "../../../../../src/components/PostFilterBox";
 import { PostFilterContextProvider } from "../../../../../src/context/PostFilterContext";
@@ -13,12 +14,29 @@ const PostCategoryPage: NextPage = () => {
   const router = useRouter();
   const { category_group, category } = router.query;
 
+  const crumbs = [{ title: "Główna", href: "/" }];
+
+  if (typeof category_group == "string") {
+    crumbs.push({
+      title: category_group,
+      href: `/posts/${category_group}`,
+    });
+  }
+
+  if (typeof category == "string") {
+    crumbs.push({
+      title: category,
+      href: `/posts/category/${category}`,
+    });
+  }
+
   return (
     <ContentWrapper>
       <PostFilterContextProvider>
+        <BreadCrumbs crumbs={crumbs} />
         <CollumnWrapper>
           <LeftCollumn>
-            PostCategoryPage {`${category_group} -> ${category}`}
+           WYNIKI WYSZUKIWANIA
           </LeftCollumn>
           <RightCollumn>
             <PostFilterBox />
--- renderer/pages/posts/category/[category_group]/index.tsx
+++ renderer/pages/posts/category/[category_group]/index.tsx
@@ -1,12 +1,49 @@
 import { NextPage } from "next";
 import { useRouter } from "next/router";
 import React from "react";
+import { BreadCrumbs } from "../../../../src/components/BreadCrumbs";
+import CategoryTree from "../../../../src/components/CategoryTree";
+import PostFilterBox from "../../../../src/components/PostFilterBox";
+import { PostFilterContextProvider } from "../../../../src/context/PostFilterContext";
+import { CollumnWrapper } from "../../../../src/layout/CollumnWrapper";
+import { ContentWrapper } from "../../../../src/layout/ContentWrapper";
+import { LeftCollumn } from "../../../../src/layout/LeftCollumn";
+import RightCollumn from "../../../../src/layout/RightCollumn";
 
 const PostCategoryGroupPage: NextPage = () => {
   const router = useRouter();
-  const { category_group } = router.query;
+  const { category_group, category } = router.query;
 
-  return <div>PostCategoryGroupPage {category_group}</div>;
+  const crumbs = [{ title: "Główna", href: "/" }];
+
+  if (typeof category_group == "string") {
+    crumbs.push({
+      title: category_group,
+      href: `/posts/${category_group}`,
+    });
+  }
+
+  if (typeof category == "string") {
+    crumbs.push({
+      title: category,
+      href: `/posts/category/${category}`,
+    });
+  }
+
+  return (
+    <ContentWrapper>
+      <PostFilterContextProvider>
+        <BreadCrumbs crumbs={crumbs} />
+        <CollumnWrapper>
+          <LeftCollumn>WYNIKI WYSZUKIWANIA</LeftCollumn>
+          <RightCollumn>
+            <PostFilterBox />
+            <CategoryTree />
+          </RightCollumn>
+        </CollumnWrapper>
+      </PostFilterContextProvider>
+    </ContentWrapper>
+  );
 };
 
-export default PostCategoryGroupPage
+export default PostCategoryGroupPage;
