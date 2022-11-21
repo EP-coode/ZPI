5156a3bfd67a625bd39c3747bb968c420b6052b7
Ernest Przybył <ernestprzybyl@gmail.com>
2022-11-21 15:20:49 +0100

    change post details colapse breakpoint; add remark gfm

--- renderer/src/layout/RightCollumn.tsx
+++ renderer/src/layout/RightCollumn.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const RightCollumn = ({ children, className }: Props) => {
   return (
-    <div className={`h-full grow py-1.5 md:px-1.5 flex flex-col gap-3 col-start-1 row-start-1 sm:col-start-2 ${className}`}>
+    <div className={`h-full grow py-1.5 md:px-1.5 flex flex-col gap-3 col-start-1 row-start-1 md:col-start-2 ${className}`}>
       {children}
     </div>
   );
