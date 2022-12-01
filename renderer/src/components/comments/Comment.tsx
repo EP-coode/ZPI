77d8a4db10ec0d1ea7f1dc7b37e9ed53b0366972
Ernest Przybył <ernestprzybyl@gmail.com>
2022-12-02 00:36:38 +0100

    add delete proggres indicator, add mobile support

--- renderer/src/components/comments/Comment.tsx
+++ renderer/src/components/comments/Comment.tsx
@@ -5,14 +5,23 @@ import { formatDate } from "../../utils/dateFormating";
 import { LikesCounter } from "../LikesCounter";
 import { likeService } from "../../services/api/LikeService";
 import { isLoggedIn, getUserId } from "../../utils/auth";
+import { LoadingState } from "../../common/LoadingState";
+import classNames from "classnames";
 
 type Props = {
   comment: Comment;
-  onDelete: (x: number) => {};
+  onDelete: (x: number) => Promise<any>;
 };
 
 const CommentBox = ({ comment, onDelete }: Props) => {
   const [showDeleteButton, setShowDeleteButton] = useState<boolean>(false);
+  const [deleteStatus, setDeleteStatus] = useState(LoadingState.IDDLE);
+
+  const handleDeleteClick = async () => {
+    setDeleteStatus(LoadingState.LOADING);
+    await onDelete(comment.commentId);
+    setDeleteStatus(LoadingState.LOADED);
+  };
 
   useEffect(() => {
     if (isLoggedIn() && getUserId() == comment.creator.id) {
@@ -23,21 +32,27 @@ const CommentBox = ({ comment, onDelete }: Props) => {
   return (
     <div>
       <div className="flex flex-row items-center gap-2">
-        <Link href={`/posts/user/${comment.creator.id}`}>
-          <a className="btn btn-sm btn-ghost text-md">{comment.creator.name}</a>
-        </Link>
-        <span className="badge badge-ghost ">
-          {formatDate(comment.creationTime)}
-        </span>
-        <div className="flex-grow">
-          {showDeleteButton && (
-            <button
-              className="btn btn-sm btn-error mr-auto ml-0 block"
-              onClick={() => onDelete(comment.commentId)}
-            >
-              USUŃ
-            </button>
-          )}
+        <div className="flex flex-row items-center flex-wrap gap-2">
+          <Link href={`/posts/user/${comment.creator.id}`}>
+            <a className="btn btn-sm btn-ghost text-md">
+              {comment.creator.name}
+            </a>
+          </Link>
+          <span className="badge badge-ghost ">
+            {formatDate(comment.creationTime)}
+          </span>
+          <div className="flex-grow">
+            {showDeleteButton && (
+              <button
+                className={classNames("btn btn-sm btn-error mr-auto ml-0", {
+                  loading: deleteStatus == LoadingState.LOADING,
+                })}
+                onClick={handleDeleteClick}
+              >
+                USUŃ
+              </button>
+            )}
+          </div>
         </div>
         <div className="mr-0 ml-auto basis-32">
           <LikesCounter
@@ -52,7 +67,7 @@ const CommentBox = ({ comment, onDelete }: Props) => {
         </div>
       </div>
 
-      <div className="prose mx-10">{comment.content}</div>
+      <div className="prose mx-10 mt-5">{comment.content}</div>
     </div>
   );
 };
